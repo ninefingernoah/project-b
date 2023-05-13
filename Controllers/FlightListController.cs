5abf5dd0fdb0125448039d0af3cd7cb2e1aa4ee7
ninefingernoah <1064057@hr.nl>
2023-05-13 11:21:10 +0200

    vlucht zoeken start

--- Controllers/FlightListController.cs
+++ Controllers/FlightListController.cs
@@ -15,12 +15,11 @@ public class FlightListController {
         }
     }
 
-    // TODO: Enhance the documentation. I have no clue what this method does.
     /// <summary>
-    /// Shows the flights?
+    /// Shows all the flights in the list
     /// </summary>
-    public void ShowFlights() {
-        var flights = new List<Flight>();
+    /// <param name="flights">The list of flights to show</param>
+    public void ShowFlights(List<Flight> flights) {
         // fill list with flights from database
         // for now
         Airplane airplane = new Airplane(1, 100, "Boeing je moeder");
@@ -28,7 +27,7 @@ public class FlightListController {
         // flights.Add(new Flight(2, "Amsterdam", "New York", new DateTime(2019, 1, 3), new DateTime(2019, 1, 4), airplane));
         // flights.Add(new Flight(3, "Amsterdam", "New York", new DateTime(2019, 1, 5), new DateTime(2019, 1, 6), airplane));
         FlightListView flightListView = FlightListView.Instance;
-        flightListView.Show(flights);
+        flightListView.Display(flights);
         try {
             string selection = flightListView.ViewBag["FlightListSelection"];
             int selectionInt = int.Parse(selection);
@@ -38,6 +37,52 @@ public class FlightListController {
                 return;
             }
             FlightController.Instance.ShowFlight(flights[selectionInt]);
+        }
+        catch (Exception) {
+            Console.WriteLine("Er is iets fout gegaan.");
+            // return to main menu
+        }
+    }
+
+    public void ShowFilters() {
+        FlightFilterView.Instance.Display();
+        try {
+            string selection = FlightFilterView.Instance.ViewBag["FlightFilterSelection"];
+            int selectionInt = int.Parse(selection);
+            switch (selectionInt) {
+                case 0:
+                    StringInputMenu departureDateMenu = new StringInputMenu("Voer een datum in: ");
+                    string departureDate = departureDateMenu.Run();
+                    if (!DateTime.TryParse(departureDate, out DateTime result)) {
+                        ConsoleUtils.Error("Ongeldige datum.");
+                    }
+                    FlightFilterView.Instance.ViewBag["departureDate"] = result.ToShortDateString();
+                    ShowFilters();
+                    break;
+                case 1:
+                    StringInputMenu departureMenu = new StringInputMenu("Voer een vertreklocatie in: ");
+                    FlightFilterView.Instance.ViewBag["departure"] = departureMenu.Run();
+                    ShowFilters();
+                    break;
+                case 2:
+                    StringInputMenu arrivalMenu = new StringInputMenu("Voer een aankomstlocatie in: ");
+                    FlightFilterView.Instance.ViewBag["arrival"] = arrivalMenu.Run();
+                    ShowFilters();
+                    break;
+                case 3:
+                    // ZOEK
+                    /* FlightListController.Instance.ShowFlights(FlightManager.GetFlights(
+                        DateTime.Parse(FlightFilterView.Instance.ViewBag["departureDate"]),
+                        FlightFilterView.Instance.ViewBag["departure"],
+                        FlightFilterView.Instance.ViewBag["arrival"]
+                    )); */
+                case 4:
+                    MainMenuController.Instance.ShowMainMenu();
+                    break;
+                default:
+                    Console.WriteLine("Ongeldige keuze.");
+                    break;
+            }
         }
         catch (Exception) {
             Console.WriteLine("Er is iets fout gegaan.");
