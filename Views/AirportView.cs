a5cbabb95e9ca0d43ae55639c2ceed7607f4394b
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-05-18 13:27:44 +0200

    Starting over

--- Views/AirportView.cs
+++ Views/AirportView.cs
@@ -1,41 +1,4 @@
 public class AirportView : IView
 {
-    Menu menu = new Menu("Airport Menu", new string[] {
-        "Naam van het vliegveld",
-        "Locatie van het vliegveld",
-        "Prijzen van vluchten",
-        "Overzicht",
-        "-",
-        "Confirm",
-        "Cancel"
-    });
-
-    public void Display()
-    {
-        int choice = menu.Run();
-        switch (choice)
-        {
-            case 0: // Naam van het vliegveld
-                AirportLocationView.Display();
-                break;
-            case 1: // Locatie van het vliegveld
-                break;
-            case 2: // Prijzen van vluchten
-                break;
-            case 3: // Overzicht
-                break;
-            case 4: // Confirm
-                break;
-            case 5: // Cancel
-                break;
-            default: // Should never happen.
-                break;
-        }
-    }
-
-    public void AirportNameMenu()
-    {
-        StringInputMenu menu = new("De naam van het vliegveld: ");
-        string? Vliegveldnaam = menu.Run();
-    }
+    public Dictionary<string, string> ViewBag = new Dictionary<string, string>();
 }
