a1b3c5315fa60a5639b9409851d5aa0a291dee3f
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-05-18 13:59:49 +0200

    AirportView en AirportController base gemaakt

--- Controllers/AirportController.cs
+++ Controllers/AirportController.cs
@@ -1,4 +1,79 @@
 public class AirportController
 {
-    
+    private static readonly AirportController instance = new AirportController();
+
+    public static AirportController Instance
+    {
+        get { return instance; }
+    }
+
+    public void showAirportCreationMenu()
+    {
+        AirportView.Instance.Display();
+        int choice = int.Parse(AirportView.Instance.ViewBag["AirportMenuSelection"]);
+
+        switch(choice)
+        {
+            case 0:
+                showAirportCountryInputMenu();
+                break;
+            case 1:
+                showAirportCityInputMenu();
+                break;
+            case 2:
+                showAirportNameInputMenu();
+                break;
+            case 3:
+                showFlightsAndPricesMenu();
+                break;
+            case 4:
+        }
+    }
+
+    private void showAirportCountryInputMenu()
+    {
+        StringInputMenu menu = new StringInputMenu("Vul het land in waar het vliegveld zich bevindt:");
+        string? country = menu.Run();
+        if (country == null)
+        {
+            showAirportCreationMenu();
+            return;
+        }
+
+        AirportView.Instance.ViewBag["AirportCountry"] = country!;
+        showAirportCreationMenu();
+    }
+
+    private void showAirportCityInputMenu()
+    {
+        StringInputMenu menu = new StringInputMenu("Vul de stad in waar het vliegveld zich bevindt:");
+        string? city = menu.Run();
+        if (city == null)
+        {
+            showAirportCreationMenu();
+            return;
+        }
+
+        AirportView.Instance.ViewBag["AirportCity"] = city!;
+        showAirportCreationMenu();
+    }
+
+    private void showAirportNameInputMenu()
+    {
+        StringInputMenu menu = new StringInputMenu("Vul de naam van het vliegveld in:");
+        string? name = menu.Run();
+        if (name == null)
+        {
+            showAirportCreationMenu();
+            return;
+        }
+
+        AirportView.Instance.ViewBag["AirportName"] = name!;
+        showAirportCreationMenu();
+    }
+
+    private void showFlightsAndPricesMenu()
+    {
+        FlightListController.Instance.ShowFlights();
+    }
 }
--- Views/AirportView.cs
+++ Views/AirportView.cs
@@ -1,4 +1,46 @@
 public class AirportView : IView
 {
+    private static readonly AirportView instance = new AirportView();
+    
     public Dictionary<string, string> ViewBag = new Dictionary<string, string>();
+
+    public static AirportView Instance
+    {
+        get { return instance; }
+    }
+
+    public void Display()
+    {
+        populateViewBag();
+
+        list<string> optionsList = new List<string>()
+        {
+            $"Land waar het vliegveld zich bevindt: {ViewBag["AirportCountry"]}",
+            $"Stad waar het vliegveld zich bevindt: {ViewBag["AirportCity"]}",
+            $"Naam van het vliegveld: {ViewBag["AirportName"]}",
+            "Vluchten en prijzen",
+            "-",
+            "Confirm",
+            "Annuleren"
+        };
+        string[] options = optionsList.ToArray();
+        Menu airportMenu = new Menu("Vliegveld aanmaken", options);
+        int choice = airportMenu.Run();
+        ViewBag["AirportMenuSelection"] = choice.ToString();
+    }
+
+    private void populateViewBag()
+    {
+        if(!ViewBag.ContainsKey("AirportCountry"))
+            ViewBag["AirportCountry"] = "<Vul In>";
+        if(!ViewBag.ContainsKey("AirportCity"))
+            ViewBag["AirportCity"] = "<Vul In>";
+        if(!ViewBag.ContainsKey("AirportName"))
+            ViewBag["AirportName"] = "<Vul In>";
+    }
+
+    public void ClearViewBag()
+    {
+        ViewBag.Clear();
+    }
 }
