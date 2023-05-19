d69244db415f1b168a465813c710a402f41d170d
Rens <mulderrens@outlook.com>
2023-05-19 12:26:12 +0200

    Began work on US.ID 37

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -1,45 +1,60 @@
-public sealed class ReservationController {
+public sealed class ReservationController
+{
     private static readonly ReservationController instance = new ReservationController();
 
-    static ReservationController() {
+    static ReservationController()
+    {
     }
-    private ReservationController() {
+    private ReservationController()
+    {
     }
 
     /// <summary>
     /// The singleton instance of the main menu controller. Used for accessing the controller. Thread safe.
     /// </summary>
-    public static ReservationController Instance {
-        get {
+    public static ReservationController Instance
+    {
+        get
+        {
             return instance;
         }
     }
 
-    /// <summary>
-    /// Sends the user to the seat selection view. Handles the user input.
-    /// </summary>
-    /// <param name="flight">The flight for which the user wants to select seats.</param>
-    public void ShowSeatSelection(Flight flight) {
-        SeatSelectionView.Instance.Display(flight);
-        try {
-            int selection = int.Parse(SeatSelectionView.Instance.ViewBag["SeatViewSelection"]);
-            // TODO: Change this to a switch statement.
-            if (selection == 1) {
-                // TODO:
-                // confirm seats
-                // create reservation
-                // get seats from viewbag
-                // add seats to reservation
-                // add reservation to database
-            }
-            else if (selection == 2) {
-                // return to flight details
-                FlightController.Instance.ShowFlight(flight);
-            }
+    public void AskReservation()
+    {
+        StringInputMenu menu = new StringInputMenu("Vul uw reserveringscode in: ");
+        int reservationCode;
+        try
+        {
+            reservationCode = int.Parse(menu.Run()!);
         }
-        catch (Exception) {
-            Console.WriteLine("Er is iets fout gegaan.");
-            // return to main menu
+        catch (Exception)
+        {
+            ConsoleUtils.Error("De ingevoerde reserveringscode is ongeldig.");
+            MainMenuController.Instance.ShowMainMenu();
+            return;
         }
+        Reservation reservation = ReservationManager.GetReservation(reservationCode);
+        if (reservation == null)
+        {
+            ConsoleUtils.Error("De ingevoerde reserveringscode is ongeldig.");
+            MainMenuController.Instance.ShowMainMenu();
+            return;
+        }
+        StringInputMenu emailMenu = new StringInputMenu("Vul uw emailadres in: ");
+        string email = emailMenu.Run()!;
+        if (reservation.User.Email != email)
+        {
+            ConsoleUtils.Error("Het ingevoerde emailadres is ongeldig.");
+            MainMenuController.Instance.ShowMainMenu();
+            return;
+        }
+        ShowReservation(reservation);
     }
+
+    public void ShowReservation(Reservation reservation)
+    {
+        // TODO: Show reservation
+    }
+
 }
