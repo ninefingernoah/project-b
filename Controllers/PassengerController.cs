dea2dc795c7b86110eef39ceb263ce9450a204de
max <0922018@hr.nl>
2023-05-30 13:03:05 +0200

    Create PassengerController.cs

---
+++ Controllers/PassengerController.cs
@@ -0,0 +1,45 @@
+public class PassengerController
+{
+    private static readonly PassengerController instance = new PassengerController();
+
+    static PassengerController()
+    {
+    }
+    private PassengerController()
+    {
+    }
+
+    /// <summary>
+    /// The singleton instance of the main menu controller. Used for accessing the controller. Thread safe.
+    /// </summary>
+    public static PassengerController Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+
+    public Passenger NewPassenger()
+    {
+        int Id = PassengerManager.GetNewestId();
+        
+        StringInputMenu menu = new StringInputMenu("Wat is de email van de reiziger?");
+        string? email = menu.Run();
+
+        menu = new StringInputMenu("Wat is de voornaam van de reiziger?");
+        string? FirstName = menu.Run();
+
+        menu = new StringInputMenu("Wat is de achternaam van de reiziger?");
+        string? LastName = menu.Run();
+
+        menu = new StringInputMenu("Wat is het document nummer van de reiziger?");
+        string? DocNumber = menu.Run();
+
+        DateTimeInputMenu DTmenu = new DateTimeInputMenu("Wat is de geboorte datum van de reiziger?");
+        DateTime? date = DTmenu.Run();
+
+        return new Passenger(Id, email, FirstName, LastName, DocNumber, date);
+    }
+}
