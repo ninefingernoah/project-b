36074208ee98ff70215318a742eb8ae592d1e3e8
SteinCalicher <101630793+SteinCalicher@users.noreply.github.com>
2023-05-31 20:38:11 +0200

    PopulatetestData word goed toegepast

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@
     /// <param name="args">Command line arguments.</param>
     public static void Main(string[] args) {
         DatabaseManager.CreateDatabase();
-        // PopulateTestData();
+        if (!IsTestDataPresent())
+        {
+            PopulateTestData();
+        }
         var mainMenuController = MainMenuController.Instance;
         mainMenuController.ShowMainMenu();
         // FlightController.Instance.ShowFlightEditor(FlightManager.GetFlight(1));
@@ -78,5 +81,110 @@
                 VALUES (1234, 2)
             ");
     }
+
+    public static bool IsTestDataPresent()
+{
+    // Check if users table has the test data
+    string query = "SELECT COUNT(*) FROM users WHERE email = 'admin'";
+    int userCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (userCount == 0)
+    {
+        return false;
+    }
+
+    query = "SELECT COUNT(*) FROM users WHERE email = 'johndoe@email.com'";
+    userCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (userCount == 0)
+    {
+        return false;
+    }
+
+    // Check if airports table has the test data
+    query = "SELECT COUNT(*) FROM airports WHERE name = 'Schiphol'";
+    int airportCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (airportCount == 0)
+    {
+        return false;
+    }
+
+    query = "SELECT COUNT(*) FROM airports WHERE name = 'Heathrow'";
+    airportCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (airportCount == 0)
+    {
+        return false;
+    }
+
+    // Check if airplanes table has the test data
+    query = "SELECT COUNT(*) FROM airplanes WHERE name = 'Boeing_787'";
+    int airplaneCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (airplaneCount == 0)
+    {
+        return false;
+    }
+
+    query = "SELECT COUNT(*) FROM airplanes WHERE name = 'Boeing_737'";
+    airplaneCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (airplaneCount == 0)
+    {
+        return false;
+    }
+
+    // Check if flights table has the test data
+    query = "SELECT COUNT(*) FROM flights WHERE airplane_id = 1";
+    int flightCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (flightCount == 0)
+    {
+        return false;
+    }
+
+    // Check if reservations table has the test data
+    query = "SELECT COUNT(*) FROM reservations WHERE number = '1234'";
+    int reservationCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (reservationCount == 0)
+    {
+        return false;
+    }
+
+    // Check if addresses table has the test data
+    query = "SELECT COUNT(*) FROM addresses WHERE street_number = '1'";
+    int addressCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (addressCount == 0)
+    {
+        return false;
+    }
+
+    query = "SELECT COUNT(*) FROM addresses WHERE street_number = '12'";
+    addressCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (addressCount == 0)
+    {
+        return false;
+    }
+
+    // Check if passengers table has the test data
+    query = "SELECT COUNT(*) FROM passengers WHERE email = 'johndoe@email.com' AND first_name = 'John' AND last_name = 'Doe' AND document_number = '123456789'";
+    int passengerCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (passengerCount == 0)
+    {
+        return false;
+    }
+
+    query = "SELECT COUNT(*) FROM passengers WHERE email = 'janedoe@email.com' AND first_name = 'Jane' AND last_name = 'Doe' AND document_number = '987654321'";
+    passengerCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (passengerCount == 0)
+    {
+        return false;
+    }
+
+    // Check if reservation_passengers table has the test data
+    query = "SELECT COUNT(*) FROM reservation_passengers WHERE reservation_number = '1234'";
+    int resPassengerCount = Convert.ToInt32(DatabaseManager.QueryResult(query).Rows[0][0]);
+    if (resPassengerCount == 0)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 
 }
