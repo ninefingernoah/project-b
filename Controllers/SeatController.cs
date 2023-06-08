812439b032c85cfa94cd73da56b5ba4835a9c6ea
Rens <1048912@hr.nl>
2023-06-08 18:33:02 +0200

    Fixed seat selection bug

--- Controllers/SeatController.cs
+++ Controllers/SeatController.cs
@@ -41,8 +41,8 @@ public class SeatController {
 
         // repeat for inward flight
         SeatSelectionView.Instance.SelectedSeats.Clear();
-        SeatSelectionView.Instance.SelectedSeats.AddRange(reservation.InwardSeats);
-        if (reservation.InwardFlight != null) {
+        if (reservation.InwardFlight != null && reservation.InwardSeats != null) {
+            SeatSelectionView.Instance.SelectedSeats.AddRange(reservation.InwardSeats);
             SeatSelectionView.Instance.Display(reservation.InwardFlight, reservation.Passengers.Count, reservation.Price);
             reservation.Price = SeatSelectionView.Instance.Price;
             if (SeatSelectionView.Instance.SelectedSeats.Count > 0) {
